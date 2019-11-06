29906077cb44739cf5e382b3527bfca1a7e62776
yuslianto <yuslianto56@gmail.com>
2019-11-06 10:19:33 +0700

    adding Header.js and StartGameScreen in Molekul and Pages folfer

--- App.js
+++ App.js
@@ -5,21 +5,23 @@ import {
   StyleSheet
 } from "react-native";
 
+import Header from "./src/Molekul/Header";
+import StartGameScreen from "./src/Pages/StartGameScreen";
+
 class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>App</Text>
+      <View style={styles.screen}>
+        <Header title="Guess a Number"/>
+        <StartGameScreen/>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   }
 });
 

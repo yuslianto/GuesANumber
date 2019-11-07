d7ad83eb2f281e7dbfc6dc9fd7def42fbc304038
yuslianto <yuslianto56@gmail.com>
2019-11-07 09:49:03 +0700

    extgracting the card presentational component

--- src/Pages/StartGameScreen.js
+++ src/Pages/StartGameScreen.js
@@ -7,18 +7,22 @@ import {
     StyleSheet
 } from "react-native";
 
+import Card from '../Organisme/Card';
+
+// finish video part 7
 const StartGameScreen = (props) => {
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Start a New Game!</Text>
-            <View style={styles.inputContainer}>
+
+            <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
                 <TextInput />
                 <View style={styles.buttonContainer}>
                     <Button title="Reset" onPress={()=> {}} />
                     <Button title="Confirm" onPress={()=> {}} />
                 </View>
-            </View>
+            </Card>
 
         </View>
     );
@@ -38,17 +42,6 @@ const styles = StyleSheet.create({
         width: 300,
         maxWidth: '80%',
         alignItems: 'center',
-        shadowColor: 'black',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.26,
-        shadowRadius: 6,
-        backgroundColor: 'white',
-        elevation: 10,
-        padding: 20,
-        borderRadius: 10
     },
     buttonContainer: {
         flexDirection: 'row',
